6a13dc2378df04afce19354a2b9f3281e4d3f69b
Thomas Crowe <wcrowe@g.clemson.edu>
2022-05-25 19:30:38 -0400

    worked through activity

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 class Casino {
-    // Write code here
+    constructor(name, fakeCoin) {
+        this.name = name;
+        this.timesPlayed = 0;
+        this.winnings = 0;
+        this.fakeCoin = fakeCoin;
+
+    }
+    
+    playGame(betAmount) {
+        this.timesPlayed += 1;
+        if(Math.random() <= 0.5 || this.fakeCoin == true){
+            console.log(this.name + ' Wins.');
+            this.timesPlayed = 0;
+            this.winnings = 0;
+        }else{
+            console.log('Players wins $' + betAmount * this.timesPlayed + ' from ' + this.name);
+            this.winnings += this.timesPlayed * betAmount;
+            console.log('Current winnings $' + this.winnings);
+        }
+    }
+
+    rollDice(sides) {
+        console.log(Math.floor(Math.random() * sides) + 1);
+    }
+
+   
 };
 
 // TESTS
@@ -11,20 +36,24 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
+//
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
-myBonusCasino.playGame(25);
+myBonusCasino.playGame(25); 
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
-/*
+
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
-myExtraBonusCasino.rollDie(6);
-myExtraBonusCasino.rollDie(20);
-myExtraBonusCasino.rollDie(100);
-*/
+myExtraBonusCasino.rollDice(6);
+myExtraBonusCasino.rollDice(6);
+myExtraBonusCasino.rollDice(6);
+myExtraBonusCasino.rollDice(6);
+myExtraBonusCasino.rollDice(6);
+myExtraBonusCasino.rollDice(20);
+myExtraBonusCasino.rollDice(100);
+
